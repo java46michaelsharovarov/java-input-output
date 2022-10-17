4fda980817b96d21db1a58237b84ee741bb97318
Mik <m.shac86@gmail.com>
2022-10-17 20:43:05 +0300

    HW_32 (fixed) + isOneSwapForSorted

--- src/telran/io/FileTests.java
+++ src/telran/io/FileTests.java
@@ -7,6 +7,7 @@ import static org.junit.jupiter.api.Assertions.*;
 import java.io.*;
 class FileTests {
 	
+	private static final String INDENT = "    ";
 	File file;
 	
 	@BeforeEach
@@ -24,7 +25,7 @@ class FileTests {
 	
 	@Test
 	void printDirectoryContent() throws IOException {
-		printDirectory("..", 4);
+		printDirectory("..", 1);
 	}
 
 	/**
@@ -37,39 +38,47 @@ class FileTests {
 	 *          level = -1 printing all levels
 	 */
 	private void printDirectory(String pathName, int level) {
-		printNodesWithLevels(new File(pathName), pathName, level, "   ");
-	}	
-
-	private void printNodesWithLevels(File file, String pathName, int level, String indent) {
-		System.out.printf(indent);
-		printNode(file);
-		printLevel(getNextPath(pathName, file), level - 1, indent.concat(indent));
+		File file = new File(pathName);
+		printDirectoryName(file);
+		printNodeWithLevels(file, level, INDENT);
 	}
 
-	private void printNode(File file) {
-		System.out.printf("<%s> type = %s\n", file.getName(), getTypeName(file));
+	private void printDirectoryName(File file) {
+		String path = file.getPath();
+		String absPath = file.getAbsolutePath();
+		if(path.matches("(.+\\\\\\.)|(\\.)")) {
+			absPath = absPath.substring(0, absPath.length() - 1);
+		} else if(path.matches("(.+\\\\\\.\\.)|(\\.\\.)")) {
+			absPath = absPath.replaceAll("[^\\\\.]+\\\\\\.\\.$", "");
+		}
+		System.out.printf("%s\n >>>\n", absPath);
 	}
 		
-	private void printLevel(String pathName, int level, String indent) {
+	private void printNodeWithLevels(File file, int level, String indent) {
 		if(level == 0) {
 			return;
 		}
-		File file = new File(pathName);
 		File[] fileList = file.listFiles();
 		if(fileList == null || fileList.length == 0) {
 			return;
 		}
 		for (int i = 0; i < fileList.length; i++) {
-			printNodesWithLevels(fileList[i], pathName, level, indent);
+			System.out.printf(indent);
+			printNode(fileList[i]);
+			printNodeWithLevels(getNextNode(file.getPath(), fileList[i]), level - 1, indent.concat(INDENT));
 		}		
 	}
 
-	private String getNextPath(String pathName, File file) {
-		return pathName.concat("\\").concat(file.getName());
+	private File getNextNode(String pathName, File file) {
+		return new File(pathName.concat("\\").concat(file.getName()));
 	}
 
 	private String getTypeName(File file) {
 		return file.isDirectory() ? "dir" : "file";
+	}	
+
+	private void printNode(File file) {
+		System.out.printf("<%s> type = %s\n", file.getName(), getTypeName(file));
 	}
 
 }
